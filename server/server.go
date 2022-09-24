fee73a7e5c1c9563a890277c4df9e501b507409a
JulianKlempa <72907327+JulianKlempa@users.noreply.github.com>
2022-09-25 01:52:10 +0200

    implemented https

--- server/server.go
+++ server/server.go
@@ -4,11 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func StartServer() {
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprint(res, "Hello, World!")
 	})
-	log.Fatal(http.ListenAndServe(":9000", nil))
+	baseFilePath, err := os.Getwd()
+
+	if err != nil {
+		fmt.Println("Error while reading ssl-files")
+		os.Exit(1)
+	}
+
+	fmt.Println(baseFilePath)
+
+	certificateFilePath := filepath.Join(baseFilePath, "server", "ssl", "localhost.crt")
+	keyFilePath := filepath.Join(baseFilePath, "server", "ssl", "localhost.key")
+
+	log.Fatal(http.ListenAndServeTLS(":9000", certificateFilePath, keyFilePath, nil))
 }
