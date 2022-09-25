14a3e9ba81610e2dae19e2c692c92553b1d9d593
JulianKlempa <72907327+JulianKlempa@users.noreply.github.com>
2022-09-25 15:27:34 +0200

    Upload

--- server/upload/upload.go
+++ server/upload/upload.go
@@ -1,10 +1,13 @@
 package upload
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"crypto/subtle"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 )
 
 type UploadHandler struct {
@@ -29,6 +32,30 @@ func (h UploadHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 				fmt.Println("No POST")
 				return
 			}
+			fmt.Println("Authenticated and post")
+
+			req.ParseMultipartForm(32 << 20) // limit your max input length!
+			var buf bytes.Buffer
+			// in your case file would be fileupload
+			file, header, err := req.FormFile("file")
+			if err != nil {
+				panic(err)
+			}
+			defer file.Close()
+			name := strings.Split(header.Filename, ".")
+			fmt.Printf("File name %s\n", name[0])
+			// Copy the file data to my buffer
+			io.Copy(&buf, file)
+			// do something with the contents...
+			// I normally have a struct defined and unmarshal into a struct, but this will
+			// work as an example
+			contents := buf.String()
+			fmt.Println(contents)
+			// I reset the buffer in case I want to use it again
+			// reduces memory allocations in more intense projects
+			buf.Reset()
+			// do something else
+			// etc write header
 			return
 		}
 	}
